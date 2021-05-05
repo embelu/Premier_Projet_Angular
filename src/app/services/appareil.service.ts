33dd402580fd6524e407ea2ae1f254e3ebcf7a77
Ludovic Baens <ludovic.baens@solidaris.be>
2021-05-05 19:39:03 +0200

    Create Formulaire de type Template (Sauvegarder les données + redirection vers liste des appareils)

--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -59,4 +59,17 @@ export class AppareilService {
   emitAppareilSubject() {
     this.appareilsSubject.next(this.appareils.slice());
   }
+
+  addAppareil(name: string, status: string) {
+    const appareilObject = {
+      id: 0,
+      name: '',
+      status: ''
+    };
+    appareilObject.name = name;
+    appareilObject.status = status;
+    appareilObject.id = this.appareils[(this.appareils.length - 1)].id + 1;
+    this.appareils.push(appareilObject);
+    this.emitAppareilSubject();
+  }
 }
