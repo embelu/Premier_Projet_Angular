977b45de032655c42b4c21fe7de9653307b6eeff
Ludovic Baens <ludovic.baens@solidaris.be>
2021-05-05 15:54:09 +0200

    Add Routing (Menu de navigation + Routes + Import RouterModule.forRoot(appRoutes)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,14 @@ import { FormsModule } from '@angular/forms';
 import { AppareilService } from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'appareils', component: AppareilViewComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AppareilViewComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -21,7 +29,8 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AppareilService],
   bootstrap: [AppComponent]
