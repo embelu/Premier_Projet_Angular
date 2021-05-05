bc29611cf82f4739d2ec9ead5fe93f77ce016dc4
Ludovic Baens <ludovic.baens@solidaris.be>
2021-05-05 18:16:38 +0200

    Add Observables (compteur temps connecté)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { AppareilService } from './services/appareil.service';
+import { Component, OnInit } from "@angular/core";
+import { interval } from "rxjs";
+
+
 
 @Component({
   selector: 'app-root',
@@ -8,10 +10,30 @@ import { AppareilService } from './services/appareil.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private appareilService: AppareilService) {
+  secondes: number;
+
+  constructor() {
   }
 
   ngOnInit(): void {
+    // Création de notre Observable
+    const counter = interval(1000);
+
+    // Souscription à notre Observable
+    counter.subscribe(
+      // Se déclenche à chaque émission de l'observable
+      (value) => {
+        this.secondes = value;
+      },
+      // Gestion des erreurs éventuelles
+      (error) => {
+        console.log('Uh-oh, an error occurred! : ' + error);
+      },
+      // Déclenchement si l'observable s'achève
+      () => {
+        console.log('Observable complete!');
+      }
+    );
   }
 
 
