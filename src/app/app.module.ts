21f00072d376e596c6f5748b88bcfe6e9401fa4b
Ludovic Baens <ludovic.baens@solidaris.be>
2021-05-05 19:18:31 +0200

    Create Formulaire de type Template (Sans sauvegarde mais affichage à la Console)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,12 @@ import { AuthService } from './services/auth.service';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { AuthGuard } from './services/auth-guard.service';
+import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
 
 const appRoutes: Routes = [
   { path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
   { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
+  { path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent },
   { path: 'auth', component: AuthComponent },
   { path: '', component: AppareilViewComponent },
   { path: 'not-found', component: NotfoundComponent },
@@ -33,7 +35,8 @@ const appRoutes: Routes = [
     AuthComponent,
     AppareilViewComponent,
     SingleAppareilComponent,
-    NotfoundComponent
+    NotfoundComponent,
+    EditAppareilComponent
   ],
   imports: [
     BrowserModule,
