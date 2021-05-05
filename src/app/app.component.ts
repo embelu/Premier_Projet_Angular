505e7aed01e9ae8bd8e9a1121903206293975d27
Ludovic Baens <ludovic.baens@solidaris.be>
2021-05-05 15:12:51 +0200

    Refactorisation en vue de la mise en place du Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,39 +7,12 @@ import { AppareilService } from './services/appareil.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  isAuth = false;
-  appareils: any[];
-
-  lastUpdate = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  });
 
   constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
   }
+
   ngOnInit(): void {
-    this.appareils = this.appareilService.appareils;
   }
 
-  onAllumer() {
-    console.log('On allume tout !');
-    this.appareilService.switchOnAll();
-  }
 
-  onEteindre() {
-    if (confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
-      this.appareilService.switchOffAll();
-    } else {
-      return null;
-    }
-  }
 }
